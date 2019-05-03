08141cdd5221f6487ec0eb5b21fdc913485e6f70
CeShine Lee <shuanck@gmail.com>
2019-05-04 00:50:47 +0800

    Set up CORS

--- api.py
+++ api.py
@@ -4,6 +4,7 @@ from typing import List
 import functools
 
 import uvicorn
+from starlette.middleware.cors import CORSMiddleware
 
 from summa_score_sentences import summarize as summarize_textrank
 from summa_score_sentences_xling import summarize_xling
@@ -28,6 +29,21 @@ class HighlightResults(BaseModel):
 
 
 app = FastAPI()
+
+origins = [
+    # "http:localhost",
+    # "http:localhost:8080",
+    # "http:localhost:3000",
+    "*"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
